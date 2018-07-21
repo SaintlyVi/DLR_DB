4fd251ff339bbcd66cd97ee09cf3145ba1545caf
SaintlyVi <wiebke.toussaint@gmail.com>
2018-07-21 22:40:22 +0200

    added clusterwin.bat

---
+++ clusterwin.bat
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set root=C:\Users\CKAN\Anaconda3
+call %root%\Scripts\activate.bat %root%
+
+clear
+echo "Ready to roll."
+
+python runClusters.py exp3_kmeans -top 5 -skip 2
+python runClusters.py exp3_som -top 5
+python runClusters.py exp3_som+kmeans -top 5
